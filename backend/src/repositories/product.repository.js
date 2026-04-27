d23c66c93518d83046c326040fb3e67bb216ef80
JonathanTubac <javiertubac1290.e@gmail.com>
2026-04-27 13:29:09 -0600

    feat(backend-products): Added repository for products

---
+++ backend/src/repositories/product.repository.js
@@ -0,0 +1,40 @@
+import { pool } from "../config/db.js";
+
+export const findAll = async () => {
+    const { rows } = await pool.query(`
+        SELECT *
+        FROM productos
+    `)
+
+    return rows
+};
+
+export const findById = async (id) => {
+    const { rows } = await pool.query(`
+        SELECT p.id, p.nombre, p.precio, p.stock, c.nombre AS categoria FROM productos p 
+        JOIN categorias c ON p.id_categoria = c.id
+        WHERE p.id = $1
+    `, [id])
+
+    return rows[0]
+};
+
+export const create = async ({ name, price, stock, id_categoria }) => {
+    const { rows } = await pool.query(`
+        INSERT INTO productos (nombre, precio, stock, id_categoria)
+        VALUES ($1, $2, $3, $4) RETURNING *
+    `, [name, price, stock, id_categoria])
+
+    return rows[0]
+};
+
+export const update = async (id, {name, price, stock, id_categoria}) => {
+    const {rows} = await pool.query(`
+        UPDATE productos 
+        SET nombre = $1, precio = $2, stock =$3, id_categoria = $4
+        WHERE id = $5
+        RETURNING *
+    `, [name, price, stock, id_categoria, id])
+
+    return rows[0]
+}
