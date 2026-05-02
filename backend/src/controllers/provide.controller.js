d33d5628aaecde13689fb01dbf71ad16da7c054f
JonathanTubac <javiertubac1290.e@gmail.com>
2026-05-01 23:10:35 -0600

    feat(backend-provide): Added repository, service and controller

---
+++ backend/src/controllers/provide.controller.js
@@ -0,0 +1,20 @@
+import * as provideService from '../services/provide.service.js'
+
+export const getAll = async (req, res, next) => {
+    try {
+        const provides = await provideService.getAllProvides();
+        res.status(200).json({ success: true, data: provides });
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const create = async (req, res, next) => {
+    try {
+        const { id_prov, id_prod, amount } = req.body;
+        const provide = await provideService.createProvide({ id_prov, id_prod, amount });
+        res.status(201).json({ success: true, data: provide });
+    } catch (err) {
+        next(err);
+    }
+}
